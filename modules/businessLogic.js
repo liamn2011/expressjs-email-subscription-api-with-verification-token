22ee39c6236965b9d1e1f2b16b66c645da26012c
liamn2011 <liamn2011@gmail.com>
2023-09-14 20:16:57 +0100

    verify message changes

--- modules/businessLogic.js
+++ modules/businessLogic.js
@@ -14,12 +14,12 @@ exports.subscribeUser = async (email) => {
 		if (customersArray.length == 0) {
 			const result = await createUser(email);
 			console.log(result.status);
-			return utilities.jsonResponse({ message: "Congratulations, you've become a muttlifer" }, 200);
+			return utilities.jsonResponse({ success: true, message: "Congratulations, you've become a muttlifer", redirect: "/subscribed" }, 200);
 		} else {
 			if (result.responseBody.customers[0].tags == "Verified" && result.responseBody.customers[0].email == email) {
-				return utilities.jsonResponse({ message: "You've already registered" }, 200);
+				return utilities.jsonResponse({ success: false, message: "You've already registered", redirect: "/" }, 200);
 			} else {
-				return utilities.jsonResponse({ message: "You need to verify your email address" }, 200);
+				return utilities.jsonResponse({ success: false, message: "You need to verify your email address", redirect: "/" }, 200);
 			}
 		}
 	} catch (error) {
@@ -49,15 +49,40 @@ exports.verifySubscriber = async (email, token) => {
 		const result = await shopifyApi.getCustomers(email);
 		const customersArray = result.responseBody.customers[0];
 		if (!customersArray) {
-			return utilities.jsonResponse({ errorCode: 3, error: "No customer found with this email." }, 200);
+			return utilities.jsonResponse(
+				{
+					success: false,
+					message: "Unfortunately, we cannot find a subscriber with this email address. We'll redirect to the muttlife.co.uk web page to subscribe.",
+					redirect: "/",
+				},
+				200
+			); //No customer found with this email.
 		}
 		if (customersArray.tags == token && customersArray.email == email) {
 			const updateResult = await shopifyApi.updateCustomers(result.responseBody.customers[0].id);
-			return updateResult;
+			return utilities.jsonResponse(
+				{ success: true, message: "Hello and Thank you for verifying your email address. You'll be redirected shortly", redirect: "/subscribed" },
+				200
+			); //Congratulations, you've become a muttlifer
 		} else if (customersArray.tags == "Verified" && customersArray.email == email) {
-			return utilities.jsonResponse({ message: "You've already verified, Thank you for subscribing" }, 200);
+			return utilities.jsonResponse(
+				{
+					success: true,
+					message: "Hello again! You've already verified, but Thank you for re-verifying. You'll be redirected shortly",
+					redirect: "/subscribed",
+				},
+				200
+			); //You've already verified, Thank you for subscribing
 		} else {
-			return utilities.jsonResponse({ message: "Email & Token does not match" }, 200);
+			return utilities.jsonResponse(
+				{
+					success: false,
+					message:
+						"Umm, there appears to be a problem with verifying your mailing address. You'll be shortly redirected to the muttlife.co.uk web page where you can resubscribe",
+					redirect: "/",
+				},
+				200
+			); //Email & Token does not match
 		}
 	} catch (error) {
 		console.error("An error occurred:", error);
